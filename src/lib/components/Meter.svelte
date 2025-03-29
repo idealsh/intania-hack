5094c5a1981d5d896cd692af6bb2cc9e48d8bfca
idealsh <141259309+idealsh@users.noreply.github.com>
2025-03-29 15:37:11 +0700

    feat: added room page and assets

---
+++ src/lib/components/Meter.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Meter, useId } from "bits-ui";
+
+  interface Props {
+    label: string;
+    min: number;
+    max: number;
+    value: number;
+    unit?: string;
+  }
+
+  const { label, min, max, value, unit }: Props = $props();
+
+  const labelId = useId();
+
+  const usedPercentage = $derived((value / max) * 100);
+  const percentageRemaining = $derived(100 - usedPercentage);
+
+  // const color = $derived.by(() => {
+  //   if (percentageRemaining < 15) return "bg-red-500 dark:bg-red-400";
+  //   if (percentageRemaining < 35) return "bg-orange-500 dark:bg-orange-400";
+  //   if (percentageRemaining < 50) return "bg-yellow-500 dark:bg-yellow-400";
+  //   return "bg-green-500 dark:bg-green-400";
+  // });
+</script>
+
+<div class="flex w-full flex-col gap-2">
+  <div class="flex items-center justify-between font-medium">
+    <span id={labelId}>{label}</span>
+    <span>{value} / {max}{unit ? " " + unit : ""}</span>
+  </div>
+  <Meter.Root
+    aria-labelledby={labelId}
+    aria-valuetext="{value} out of {max}"
+    {value}
+    {min}
+    {max}
+    class="clip-pixel shadow-mini-inset relative z-10 h-[15px] overflow-hidden bg-green-900/20"
+  >
+    <div class="border-pixel-progress clip-pixel absolute inset-0 z-0 h-full w-full"></div>
+    <div
+      class="shadow-mini-inset clip-pixel absolute -z-1 h-full w-full flex-1 bg-green-500 transition-all duration-1000 ease-in-out"
+      style="transform: translateX(-{100 - (100 * (value ?? 0)) / max}%)"
+    ></div>
+  </Meter.Root>
+</div>
---
+++ src/routes/room/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Button from "$lib/components/Button.svelte";
+  import Meter from "$lib/components/Meter.svelte";
+</script>
+
+<div class="relative flex min-h-screen items-stretch justify-center bg-stone-900 text-white">
+  <div class="font-pixel h-full w-1/2 flex-grow space-y-12 pt-8 pl-8">
+    <div class="w-64">
+      <div class="">
+        <p class="mb-2 text-xl font-bold">User</p>
+        <Meter label="Level 10" unit="XP" min={0} max={100} value={42}></Meter>
+      </div>
+    </div>
+    <div>
+      <p class="mb-2 text-xl">Daily tasks:</p>
+      <ul>
+        <li>- Do 1 challenge</li>
+      </ul>
+    </div>
+  </div>
+  <div class="flex flex-grow items-center select-none">
+    <div class="relative h-fit">
+      <img src="/rooms/room1.png" alt="" srcset="" class="w-192 object-contain" />
+      <img src="/boyidle.gif" alt="" class="absolute right-[30%] bottom-[20%] h-24" />
+    </div>
+  </div>
+  <div class="w-1/2 flex-grow">a</div>
+  <div class="absolute bottom-12 left-1/2 -translate-x-1/2">
+    <Button class="w-64 text-2xl font-bold uppercase">Go adventure!</Button>
+  </div>
+</div>
