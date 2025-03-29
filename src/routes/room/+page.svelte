8d5514b9a2284ca64953fdeab9236389689fe82c
idealsh <141259309+idealsh@users.noreply.github.com>
2025-03-29 21:19:55 +0700

    feat: improved room ui and added streak

--- src/routes/room/+page.svelte
+++ src/routes/room/+page.svelte
@@ -4,33 +4,45 @@
 </script>
 
 <div
-  class="flex min-h-screen flex-col items-center justify-center bg-stone-900 to-30% px-8 pt-6 text-white"
+  class="flex min-h-screen flex-col items-center justify-center bg-stone-900 to-30% px-12 py-8 text-white"
 >
-  <div class="flex w-full flex-grow flex-col sm:flex-row">
-    <div class="font-pixel h-full flex-grow space-y-4 sm:w-1/2 sm:space-y-12">
+  <div
+    class="font-pixel relative flex w-full flex-grow flex-col justify-start gap-y-6 sm:flex-row sm:justify-between sm:gap-y-12"
+  >
+    <div class="h-full sm:w-1/2">
       <div class="w-64">
-        <div class="">
-          <p class="mb-2 text-xl font-bold">User</p>
+        <div class="space-y-4">
+          <p class="text-xl font-bold">User</p>
           <Meter label="Level 10" unit="XP" min={0} max={100} value={42}></Meter>
+          <div class="flex -translate-x-2 items-center gap-x-1.5">
+            <img src="/fire.png" alt="" class="h-8" />
+            <span>100 days</span>
+          </div>
         </div>
       </div>
-      <div>
-        <p class="mb-2 text-xl">Daily tasks:</p>
-        <ul>
-          <li>- Explore 1 island</li>
-          <li>- Explore all islands</li>
-        </ul>
+    </div>
+    <div class="w-64">
+      <p class="mb-2 text-xl">Daily tasks:</p>
+      <ul>
+        <li>- Explore 1 island</li>
+        <li>- Explore all islands</li>
+      </ul>
+    </div>
+    <div class="inset-0 flex items-center justify-center sm:absolute sm:mt-12">
+      <div
+        class="relative aspect-square max-h-[70vh] w-128 drop-shadow-[0_0_100px_rgba(255,184,106,0.3)] select-none"
+      >
+        <img src="/rooms/room1.png" alt="" srcset="" />
+        <img
+          src="/boyidle.gif"
+          alt=""
+          class="absolute right-[25%] bottom-[12%] h-[20%] -translate-x-1/2 -translate-y-1/2"
+        />
       </div>
     </div>
-    <div class="flex flex-grow items-center select-none">
-      <div class="relative h-fit">
-        <img src="/rooms/room1.png" alt="" srcset="" class="w-192 object-contain" />
-        <img src="/boyidle.gif" alt="" class="absolute right-[30%] bottom-[20%] h-24" />
-      </div>
-    </div>
-    <div class="w-1/2 flex-grow"></div>
+    <!-- <div class="w-1/2 flex-grow"></div> -->
   </div>
-  <div class="pb-12">
+  <div class="pb-8">
     <Button class="w-64 text-2xl font-bold uppercase">Go adventure!</Button>
   </div>
 </div>
